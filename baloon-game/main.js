267005a30952f3b308760be10ed70caafc89901e
Kolade Adelaiye <cholards>
2021-12-16 12:50:19 +0000

    Pushof game

--- baloon-game/main.js
+++ baloon-game/main.js
@@ -35,3 +35,20 @@ const balloonAttack = () => {
 }
 
 balloonAttack()
+
+// actual solution 
+
+const balloonAttack = (player1, player2) => {
+    for (let i = 1; i <= 10; i++) {
+        player2.balloonCount -= player1.hitsPerMinute
+        player1.balloonCount -= player2.hitsPerMinute
+        player1.status();
+        player2.status();
+    }
+    if (player1.balloonCount > player2.balloonCount) {
+        return player1.name;
+    } else if (player2.balloonCount > player1.balloonCount)
+        return player2.name
+
+    return 'Tie'
+}
